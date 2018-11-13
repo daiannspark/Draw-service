2f3ee6edf1ac0d2b9f206d02efe23dbda036caf0
Diana Markevich <44816231+daiannspark@users.noreply.github.com>
2018-11-13 14:46:44 +0200

    final
    ready to present

--- resources/views/pages/email.blade.php
+++ resources/views/pages/email.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
-    <div id="emailIsSent">Thank you, I will get in touch with you soon!</div>
+    <div id="emailIsSent">
+        <h1> Thank you, I will get in touch with you soon! </h1>
+    </div>
 
 @stop
