3f63f2dd6329a7d0bdc21c612bc215cd3af1cff8
Diana Markevich <44816231+daiannspark@users.noreply.github.com>
2018-11-13 09:13:30 +0200

    Added images to admin

--- resources/views/pages/admin_images.blade.php
+++ resources/views/pages/admin_images.blade.php
@@ -9,9 +9,12 @@
     {{ $image->fullname }}<br />
     {{ $image->email }}<br />
     {{ $image->phone }}<br />
+    <img class="imagesItem" src="<?=str_replace('public', '/storage', $image->image)?>"><br />
     {{ $image->comments }}<br />
-
+    {{-- @php
+        str_replace("world","Peter","Hello world!")
+    @endphp --}}
     <br />
-        
+    
     @endforeach 
 @stop
